05e62cc76de6cbbe36c5c6dddf94896fc0835574
Manuel Kremkow <manuel.kremkow@gmx.de>
2017-03-22 12:42:39 +0100

    my first commit

---
+++ swiftAppOnlineKurs/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  swiftAppOnlineKurs
+//
+//  Created by Manuel Kremkow on 22.03.17.
+//  Copyright © 2017 Manuel Kremkow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var label: UILabel!
+    var counter = 0;
+    @IBAction func button(_ sender: AnyObject) {
+        counter += 1;
+        label.text = String(counter);
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
